d23f3a116241a06f7b073fa08346d551abb63b52
Penn Bauman <pennbauman@protonmail.com>
2020-07-26 02:20:05 -0400

    removed unused file and organized install script

--- dnf.py
+++
@@ -1,6 +0,0 @@
-# DNF - Package Manager Manager
-#   Penn Bauman (pennbauman@protonmail.com)
-#   https://github.com/pennbauman/pmm
-
-def update(package):
-    print("> dnf update")
--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 install_dir=/usr/share/pmm
 
 sudo cp pmm.sh /bin/pmm
-sudo cp colors.py $install_dir
+sudo chmod 755 /bin/pmm
 
 sudo mkdir -p $install_dir
 sudo cp pmm.py $install_dir
-sudo chmod 755 /bin/pmm
+sudo cp colors.py $install_dir
 sudo cp help_menu.py $install_dir
 sudo cp -r managers $install_dir
